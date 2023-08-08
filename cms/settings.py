8c9b5ae1e4163f4c5f50766511e8404c2ce9b6f9
ale266 <aleyster266@fpuna.edu.py>
2023-08-08 18:12:26 -0400

    definicion de Cuentas de usuario

--- cms/settings.py
+++ cms/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cmsapp',
     'ckeditor',
+    'fontawesomefree',
     'userprofile',
 ]
 
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:pk>', views.profile, name = 'profile'),
+    path('profile/<str:pk>', views.profile, name = 'profile'),
+    path('account',  views.account, name = 'account'),
 ]
