d182bc380eede1c8a6613c702cce74f4b237b4ee
ale266 <aleyster266@fpuna.edu.py>
2023-08-10 13:23:08 -0400

    Login SignUp y Logout

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.forms import ModelForm
 from .models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+
 
 class UpdateProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         exclude = ['user']
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
