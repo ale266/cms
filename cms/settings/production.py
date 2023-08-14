89886552a7993bbcef210087bb4cd091245aa33e
ale266 <aleyster266@fpuna.edu.py>
2023-08-14 17:23:24 -0400

    production settings

--- cms/settings/production.py
+++ cms/settings/production.py
@@ -1,5 +1,8 @@
 from .base import *
 import os
+from dotenv import load_dotenv
+
+load_dotenv(Path(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
