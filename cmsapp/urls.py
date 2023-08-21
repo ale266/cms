5a39289f3f9593e263571043fce6a2f49406fc1a
ale266 <aleyster266@fpuna.edu.py>
2023-08-21 15:59:07 -0400

    Filtro por categoria

--- cmsapp/urls.py
+++ cmsapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:categoria>', views.indexCat, name='indexCat'),
+    # path('test/<str:categoria>', views.pregunta, name='test'),
+    path('dislike-post/<str:slug>', views.dislikePost, name= 'dislike'),
     path('article/<str:slug>', views.detail, name= 'detail'),
     path('create-post', views.createPost, name= 'create'),
     path('update-post/<str:slug>', views.updatePost, name= 'update'),
