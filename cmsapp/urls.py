31fa1410ac91211642d71d0ff018fa5659873d60
ale266 <aleyster266@fpuna.edu.py>
2023-08-21 18:07:29 -0400

    a

--- cmsapp/urls.py
+++ cmsapp/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
     path('category/<str:categoria>', views.indexCat, name='indexCat'),
-=======
-    path('<str:categoria>', views.indexCat, name='indexCat'),
->>>>>>> 5a39289f3f9593e263571043fce6a2f49406fc1a
     # path('test/<str:categoria>', views.pregunta, name='test'),
     path('dislike-post/<str:slug>', views.dislikePost, name= 'dislike'),
     path('article/<str:slug>', views.detail, name= 'detail'),
