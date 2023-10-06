825c4335535de97157368c707bcf09d6eddaab6d
Alex <alexisroarce2000@gmail.com>
2023-10-05 21:01:37 -0300

    control

--- cmsapp/views.py
+++ cmsapp/views.py
@@ -43,7 +43,68 @@ def indexCat(request, categoria):
     context = {'posts': posts, 'categories': categories, 'categoriesPosts': categoriesPosts} #crea un diccionario "context" y lo pasa a la plantilla HTML
     return render(request, 'cmsapp/indexCategory.html', context) #renderiza al indexCategory.html y devuelve una respuesta HTTP
 
+<<<<<<< HEAD
 
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    session_key = request.session.session_key
+     # Verifica si ya se ha registrado la visualización en esta sesión
+    if not request.session.get(f'post_{post.post_id}_viewed', False):
+        # Incrementa el contador de visualizaciones
+        post.views += 1
+        post.save()
+        # Registra que esta sesión ha visto el post
+        request.session[f'post_{post.post_id}_viewed'] = True
+        
+    posts = Post.objects.exclude(post_id__exact=post.post_id)[:5] #para mostrar en recent posts solo 5 post
+    total_likes = post.total_likes()
+    total_dislikes = post.total_dislikes()
+    if request.method == 'POST':
+        comment_form = PostCommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.success(request, 'Comentario agregado exitosamente.')
+            return redirect('detail', slug=slug)
+        else:
+            messages.error(request, 'Error al agregar el comentario. Por favor, verifica los datos.')
+    else:
+        comment_form = PostCommentForm()
+
+    context = {
+        'post': post,
+        'comment_form': comment_form, 'posts' : posts, 'total_likes': total_likes, 'total_dislikes': total_dislikes
+    }
+
+    return render(request, 'cmsapp/detail.html', context)
+
+
+def delete_comment(request):
+    if request.method == 'POST':
+        comment_id = request.POST.get('comment_id')
+        post_id = request.POST.get('post_id')
+        comment = get_object_or_404(Comment, id=comment_id)
+        
+        # Verificar si el usuario tiene permiso para eliminar el comentario
+        if comment.author == request.user:
+            comment.delete()
+            # Redirigir a la misma página después de eliminar el comentario
+            return redirect(request.META.get('HTTP_REFERER', 'home'), pk=post_id)  # Redirigir a la página anterior, si no está disponible, redirige a 'home'
+        else:
+            # Manejar el caso si el usuario no tiene permiso para eliminar el comentario
+            # Por ejemplo, puedes mostrar un mensaje de error o redirigir a otra página
+            pass
+    
+    # Manejar la situación si alguien trata de acceder a esta vista directamente sin el método POST
+    return redirect(request.META.get('HTTP_REFERER', 'home'), pk=post_id)  # Redirigir a la página anterior, si no está disponible, redirige a 'home'
+
+"""""
+=======
+#Función que  se encarga de mostrar los detalles de una publicación en particular, incluyendo su contenido, la lista de publicaciones recientes,
+#y la cantidad de "me gusta" y "no me gusta" en esa publicación.
+>>>>>>> a390c21af0a194226ff49680e670e27fad8cb933
 def detail(request, slug):
     post  = Post.objects.get(slug = slug ) #buscamos un objeto "post" en la BD que tenga el slug proporcionado
     #excluimos el post actual de la lista "recent post" para asegurarnos de que no se muestre en recent posts
@@ -51,9 +112,10 @@ def detail(request, slug):
     #realizamos el conteo de "likes" y "dislikes"
     total_likes = post.total_likes()
     total_dislikes = post.total_dislikes()
+<<<<<<< HEAD
     context = {'post': post , 'posts' : posts, 'total_likes': total_likes, 'total_dislikes': total_dislikes}
     return render (request , 'cmsapp/detail.html', context )
-
+"""""
 #Comentarios
 class PostDetailView(generic.DetailView):#Vista Detallada para el modelo Post -- comentarios
     model = Post
@@ -108,7 +170,12 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'detail.html', {'post': post})
 
+=======
+    context = {'post': post , 'posts' : posts, 'total_likes': total_likes, 'total_dislikes': total_dislikes} #crea un diccionario "context" y lo pasa a la plantilla HTML
+    return render (request , 'cmsapp/detail.html', context ) #renderiza a detail.html y devuelve una respuesta HTTP
+>>>>>>> a390c21af0a194226ff49680e670e27fad8cb933
 
+@permission_required('cmsapp.add_post') #vista que requiere permisos específicos
 def createPost(request):
     profile = request.user.userprofile #obtenemos el perfil de usuario actual
     form = PostForm() #creamos una instancia del formulario
