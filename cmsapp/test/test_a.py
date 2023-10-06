b859a763a289488e323430d82edc8df830b3920c
ale266 <aleyster266@fpuna.edu.py>
2023-10-06 14:37:10 -0300

    tests

---
+++ cmsapp/test/test_a.py
@@ -0,0 +1,31 @@
+import django
+from django.test import TestCase
+# Create your tests here.
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings.development')
+
+django.setup()
+import pytest
+from cmsapp.models import Category
+
+def test_example():
+    assert 1 == 1
+
+
+def test_example2():
+    assert 1 == 1
+def test_example3():
+    assert 1 == 1
+def test_example4():
+    assert 1 == 1
+def test_example5():
+    assert 1 == 1
+def test_example6():
+    assert 1 == 1
+
+    
+@pytest.mark.django_db
+def test_create_category():
+    category = Category.objects.create(title="Software Engineering")
+    assert category.title == "Software Engineering"
