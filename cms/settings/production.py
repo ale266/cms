52e3e805610ebefe6f87194085b4186ffeabe510
ale266 <aleyster266@fpuna.edu.py>
2023-10-20 13:39:50 -0300

    Production

--- cms/settings/production.py
+++ cms/settings/production.py
@@ -20,14 +20,15 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
